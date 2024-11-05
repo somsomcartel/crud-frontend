919baaaed734e1d5d125ee34dad13c3784c4ae78
tl1l1l1s <shj721553@gmail.com>
2024-11-06 08:25:34 +0900

    feature: screen files initial setting

---
+++ lib/screens/board_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BoardScreen extends StatefulWidget {
+  const BoardScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
---
+++ lib/screens/join_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class JoinScreen extends StatelessWidget {
+  const JoinScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
